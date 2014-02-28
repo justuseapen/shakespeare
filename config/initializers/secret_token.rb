7835924e3923a1fc1ac21d1c5c79fcaa817c84fd
justuseapen <justuseapen@gmail.com>
2014-02-28 19:41:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shakespeare::Application.config.secret_key_base = 'd846d636becd1906f4204f54329cf6c362687d9506ff68ef4d9f58dcb3624bec9ef000be5db5de3702521ff203d2f59d6b0e431371271e2bd4774c22d0292edb'
